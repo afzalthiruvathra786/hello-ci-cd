e17a174d0c7a44cb285c54b18dcb2ec09a1f0487
Afzalthiruvathra786 <Afzalthiruvathra786@gmail.com>
2025-09-16 16:07:47 +0000

    first commit

---
+++ test/test_app.py
@@ -0,0 +1,8 @@
+from src.app import app
+
+def test_root():
+        client = app.test_client()
+        resp = client.get("/")
+        assert resp.status_code == 200
+        assert b"Hello, CI/CD!" in resp.data
+
