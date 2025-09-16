4c5b0d53f01cb7e46ac377dd0ca3ba5d9f2eaf4a
Afzalthiruvathra786 <Afzalthiruvathra786@gmail.com>
2025-09-16 16:33:26 +0000

    test error corrected

--- test/test_app.py
+++ test/test_app.py
@@ -1,8 +1,8 @@
 from src.app import app
 
 def test_root():
-        client = app.test_client()
-        resp = client.get("/")
-        assert resp.status_code == 200
-        assert b"Hello, CI/CD!" in resp.data
+    client = app.test_client()
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert b"Hello, CI/CD!" in resp.data
 
